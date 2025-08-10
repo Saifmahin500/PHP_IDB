d75a03e2493b571e10b6e7a6f0827bbcc3d6b4e1
saifmahin500 <saifmahin500@gmail.com>
2025-08-10 18:57:13 +0600

    front enf

--- project/E-commarce Project/index.php
+++ project/E-commarce Project/index.php
@@ -1,5 +1,52 @@
 <?php
-
-echo "Welcome front end";
-
+require_once 'partials/header.php';
+require_once 'partials/navbar.php';
 ?>
+
+<!--Hero Section -->
+<div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/07.jpg" class="d-block w-100" alt="slide1">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/06.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/08.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+
+<!--Main Layout-->
+
+<div class="container-fluid mt-4">
+	<div class="main-wrap">
+		<?php  require_once 'partials/sidebar.php'; ?>
+
+		<section class="content-area pl-3 pr-3">
+				<div class="d-flex justify-content-between align-items-center mb-3">
+					<h4 class="mb-0">Products</h4>
+					<small class="text-muted">Filter by categories from left.</small>
+				</div>
+
+		<!-- Products Grid -->
+		<div id="productGrid" class="row"></div>
+		</section>
+	</div>
+</div>
+
+<?php require_once 'partials/footer.php'; ?>
