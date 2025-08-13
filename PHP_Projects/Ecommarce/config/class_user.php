9dd2e692314639c5ad6cee736ce84ec0c48e4cd8
saifmahin500 <saifmahin500@gmail.com>
2025-08-13 12:46:21 +0600

    reset pass

--- PHP_Projects/Ecommarce/config/class_user.php
+++ PHP_Projects/Ecommarce/config/class_user.php
@@ -213,8 +213,8 @@ class USER
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'araman666@gmail.com';
-        $mail->Password = 'lkilpfvjfjtnjzdr';
+        $mail->Username = 'saifpwad@gmail.com';
+        $mail->Password = '';
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
